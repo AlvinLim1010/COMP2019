ed19c3f99e09f33e55d20a21ae455c48846cbf0b
Aiman Mohd Said <warizupguys@hotmail.com>
2021-11-28 15:51:06 +0800

    Add input box for username n password

--- LoginPage.py
+++ LoginPage.py
@@ -1,8 +1,9 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
-window = Tk()
+window = tk.Tk()
 
-loginLabel = Label(
+loginLabel = tk.Label(
     window,
     text="LOGIN",
     fg="white",
@@ -11,14 +12,28 @@ loginLabel = Label(
     height=4,
 ).grid(column=0, row=0, pady=20, padx=150)
 
-Grid.rowconfigure(window,0, weight = 1)
-Grid.columnconfigure(window,0, weight = 1)
+tk.Grid.rowconfigure(window, 0, weight=1)
+tk.Grid.columnconfigure(window, 0, weight=1)
 
-topLine = Canvas(width=1000)
+topLine = tk.Canvas(width=1000)
 topLine.create_line(20, 5, 980, 5)
 topLine.grid(row=1, column=0)
 
-botLine = Canvas(width=1000)
+label = ttk.Label(window, text = "Enter username")
+label.grid(column=0, row=1, pady=(150, 0))
+
+username = tk.StringVar()
+nameEntered = ttk.Entry(window, width=30, textvariable=username)
+nameEntered.grid(column=0, row=1, pady=(210, 0))
+
+label = ttk.Label(window, text = "Enter password")
+label.grid(column=0, row=2, pady=(0, 100))
+
+password = tk.StringVar()
+nameEntered = ttk.Entry(window, width=30, textvariable=password, show="*")
+nameEntered.grid(column=0, row=2, pady=(0, 50))
+
+botLine = tk.Canvas(width=1000)
 botLine.create_line(20, 5, 980, 5)
 botLine.grid(row=3, column=0)
 
