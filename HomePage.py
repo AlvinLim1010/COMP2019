1c12c976ba5905121123f78b328a7bf5e7519b57
AlvinElves <93361770+AlvinElves@users.noreply.github.com>
2021-12-03 15:06:47 +0800

    Added rectangle

--- HomePage.py
+++ HomePage.py
@@ -3,7 +3,7 @@ import tkinter as tk
 window = tk.Tk()
 
 
-def backToLogin():
+def toLoginPage():
     window.destroy()
     import LoginPage
 
@@ -16,7 +16,7 @@ logOutLabel = tk.Button(
     bg="grey",
     width=10,
     height=2,
-    command=backToLogin
+    command=toLoginPage
 ).grid(column=0, row=0, padx=(900, 20), pady=(5, 700))
 
 loginLabel = tk.Label(
@@ -31,21 +31,22 @@ loginLabel = tk.Label(
 tk.Grid.rowconfigure(window, 0, weight=1)
 tk.Grid.columnconfigure(window, 0, weight=1)
 
-rectangle = tk.Canvas(width=1000)
+rectangle = tk.Canvas(width=1000, height=750)
 rectangle.create_rectangle(10, 70, 950, 20)
 rectangle.create_line(15, 116, 160, 116)
-rectangle.grid(row=0, column=0, padx=(15, 15), pady=(80, 400))
+rectangle.create_rectangle(10, 645, 950, 125)
+rectangle.grid(row=0, column=0, padx=(15, 15), pady=(80, 0))
 
-homeButton = tk.Button(window, text="HOME", font=("Arial", 8), fg="white", bg="grey", width=23, height=2, )
+homeButton = tk.Button(window, text="HOME", font=("Arial", 8), fg="white", bg="grey", width=23, height=2)
 homeButton.grid(column=0, row=0, padx=(10, 665), pady=(6, 506))
 
-predictButton = tk.Button(window, text="PREDICT", font=("Arial", 8), fg="white", bg="grey", width=23, height=2, )
+predictButton = tk.Button(window, text="PREDICT", font=("Arial", 8), fg="white", bg="grey", width=23, height=2)
 predictButton.grid(column=0, row=0, padx=(110, 335), pady=(6, 506))
 
-trainButton = tk.Button(window, text="TRAIN", font=("Arial", 8), fg="white", bg="grey", width=23, height=2, )
+trainButton = tk.Button(window, text="TRAIN", font=("Arial", 8), fg="white", bg="grey", width=23, height=2)
 trainButton.grid(column=0, row=0, padx=(335, 130), pady=(6, 506))
 
-historyButton = tk.Button(window, text="HISTORY", font=("Arial", 8), fg="white", bg="grey", width=23, height=2, )
+historyButton = tk.Button(window, text="HISTORY", font=("Arial", 8), fg="white", bg="grey", width=23, height=2)
 historyButton.grid(column=0, row=0, padx=(665, 30), pady=(6, 506))
 
 instructionLabel = tk.Label(window, text="INSTRUCTIONS", font=("Arial", 15))
