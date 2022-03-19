8ee055ea138fc199bdd311dd987037c4c2792785
AlvinElves <93361770+AlvinElves@users.noreply.github.com>
2022-03-19 20:56:02 +0800

    Implemented SVR into singlePrediction

--- Software.py
+++ Software.py
@@ -6,6 +6,14 @@ from tkinter.filedialog import askopenfile
 from openpyxl import load_workbook
 from PIL import Image, ImageTk
 
+# Sklearn
+from sklearn.svm import SVR  # for building SVR model
+from sklearn.multioutput import MultiOutputRegressor  # To make the output be independent to each other
+
+# Data Manipulation
+import pandas as pd  # for data manipulation
+import numpy as np  # for data manipulation
+
 TitleFont = ("Arial", 35)
 
 class tkinterApp(tk.Tk):
@@ -39,6 +47,33 @@ class tkinterApp(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(PredictionPage)
+
+        # Get the data from excel to train the AI model
+        global updated_df
+        dataset = pd.read_excel("Excel Data File/Data_SEGP.xlsx")
+        dataset.drop("Unnamed: 18", axis=1, inplace=True)
+        dataset.drop("Unnamed: 19", axis=1, inplace=True)
+        dataset.drop("Unnamed: 20", axis=1, inplace=True)
+        dataset.drop("Unnamed: 21", axis=1, inplace=True)
+        updated_df = dataset
+        updated_df['CH4'] = updated_df['CH4'].fillna(updated_df['CH4'].median()).round(1)
+        updated_df['Volume Biogas/L'] = updated_df['Volume Biogas/L'].fillna(
+        updated_df['Volume Biogas/L'].median()).round(1)
+        updated_df['H2S'] = updated_df['H2S'].fillna(updated_df['H2S'].median()).round(1)
+        updated_df['CO2'] = updated_df['CO2'].fillna(updated_df['CO2'].median()).round(1)
+        updated_df['pH Reactor'] = updated_df['pH Reactor'].fillna(updated_df['pH Reactor'].median()).round(1)
+        updated_df['pH F'] = updated_df['pH F'].fillna(updated_df['pH F'].median()).round(1)
+        updated_df['pH Eff'] = updated_df['pH Eff'].fillna(updated_df['pH Eff'].median()).round(1)
+        updated_df['ORL gCOD/Ld'] = updated_df['ORL gCOD/Ld'].fillna(updated_df['ORL gCOD/Ld'].median()).round(1)
+        updated_df['COD F/mg/L'] = updated_df['COD F/mg/L'].fillna(updated_df['COD F/mg/L'].median()).round(1)
+        updated_df['COD Eff/mg/L'] = updated_df['COD Eff/mg/L'].fillna(updated_df['COD Eff/mg/L'].median()).round(1)
+        updated_df['COD removal'] = updated_df['COD removal'].fillna(updated_df['COD removal'].median()).round(1)
+        updated_df['BOD F/mg/L'] = updated_df['BOD F/mg/L'].fillna(updated_df['BOD F/mg/L'].median()).round(1)
+        updated_df['BOD Eff/mg/L'] = updated_df['BOD Eff/mg/L'].fillna(updated_df['BOD Eff/mg/L'].median()).round(1)
+        updated_df['BOD removal'] = updated_df['BOD removal'].fillna(updated_df['BOD removal'].median()).round(1)
+        updated_df['TSS F/mg/L'] = updated_df['TSS F/mg/L'].fillna(updated_df['TSS F/mg/L'].median()).round(1)
+        updated_df['TSS Eff/mg/L'] = updated_df['TSS Eff/mg/L'].fillna(updated_df['TSS Eff/mg/L'].median()).round(1)
+        updated_df['TSS removal'] = updated_df['TSS removal'].fillna(updated_df['TSS removal'].median()).round(1)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
@@ -205,17 +240,82 @@ class PredictionPage(tk.Frame):
         input9Entered = tk.Entry(self, width=30, command=clear(), textvariable=input9)
         input9Entered.grid(column=0, row=0, padx=(780, 0), pady=(90, 0))
 
-        CH4 = tk.Checkbutton(self, text='CH4', bg="#E4BC9E", activebackground="#E4BC9E").grid(column=0, row=0,padx=(150, 0),pady=(300, 0))
-        CO2 = tk.Checkbutton(self, text='CO2', bg="#E4BC9E", activebackground="#E4BC9E").grid(column=0, row=0,padx=(250, 0),pady=(300, 0))
-        H2S = tk.Checkbutton(self, text='H2S', bg="#E4BC9E", activebackground="#E4BC9E").grid(column=0, row=0,padx=(350, 0),pady=(300, 0))
+        CH4Check = tk.IntVar()
+        CO2Check = tk.IntVar()
+        H2SCheck = tk.IntVar()
+        CH4 = tk.Checkbutton(self, text='CH4', variable=CH4Check, bg="#E4BC9E", activebackground="#E4BC9E").grid(column=0, row=0,padx=(150, 0),pady=(300, 0))
+        CO2 = tk.Checkbutton(self, text='CO2', variable=CO2Check, bg="#E4BC9E", activebackground="#E4BC9E").grid(column=0, row=0,padx=(250, 0),pady=(300, 0))
+        H2S = tk.Checkbutton(self, text='H2S', variable=H2SCheck, bg="#E4BC9E", activebackground="#E4BC9E").grid(column=0, row=0,padx=(350, 0),pady=(300, 0))
 
         doPredictionButton = tk.Button(self, text="DO PREDICTION", fg="#4F3D2F", bg="white", width=23, height=2, bd=0,
-                                       command=lambda: controller.show_frame(OutputPage))
+                                       command=lambda: predictSingleOutput(input1.get(), input2.get(), input3.get(),
+                                                                           input4.get(), input5.get(), input6.get(),
+                                                                           input7.get(), input8.get(), input9.get(),
+                                                                           CH4Check.get(), CO2Check.get(), H2SCheck.get()))
         doPredictionButton.grid(column=0, row=0, padx=(340, 80), pady=(520, 20))
 
         clearButton = tk.Button(self, text="CLEAR ALL", fg="#4F3D2F", bg="white", width=13, height=1, bd=0,
                                 command=lambda: clear())
         clearButton.grid(column=0, row=0, padx=(850, 80), pady=(520, 20))
+
+        def predictSingleOutput(ph_input1, ph_input2, ph_input3, cod_input1, cod_input2, cod_input3, bod_input1,
+                                bod_input2, bod_input3, ch4_input, co2_input, h2s_input):
+            xInput = [[]]
+            x = []
+            y = []
+            xInput = np.asarray(xInput)
+            x = np.array(x)
+            y = np.array(y)
+
+            if ph_input1 != '':
+                xInput = np.concatenate((xInput, [[float(ph_input1)]]), 1)
+                x = np.concatenate((x, ['pH Reactor']))
+            if ph_input2 != '':
+                xInput = np.concatenate((xInput, [[float(ph_input2)]]), 1)
+                x = np.concatenate((x, ['pH F']))
+            if ph_input3 != '':
+                xInput = np.concatenate((xInput, [[float(ph_input3)]]), 1)
+                x = np.concatenate((x, ['pH Eff']))
+            if cod_input1 != '':
+                xInput = np.concatenate((xInput, [[float(cod_input1)]]), 1)
+                x = np.concatenate((x, ['COD F/mg/L']))
+            if cod_input2 != '':
+                xInput = np.concatenate((xInput, [[float(cod_input2)]]), 1)
+                x = np.concatenate((x, ['COD Eff/mg/L']))
+            if cod_input3 != '':
+                xInput = np.concatenate((xInput, [[float(cod_input3)]]), 1)
+                x = np.concatenate((x, ['COD removal']))
+            if bod_input1 != '':
+                xInput = np.concatenate((xInput, [[float(bod_input1)]]), 1)
+                x = np.concatenate((x, ['BOD F/mg/L']))
+            if bod_input2 != '':
+                xInput = np.concatenate((xInput, [[float(bod_input2)]]), 1)
+                x = np.concatenate((x, ['BOD Eff/mg/L']))
+            if bod_input3 != '':
+                xInput = np.concatenate((xInput, [[float(bod_input3)]]), 1)
+                x = np.concatenate((x, ['BOD removal']))
+
+            if ch4_input == 1:
+                y = np.concatenate((y, ['CH4']))
+            if co2_input == 1:
+                y = np.concatenate((y, ['CO2']))
+            if h2s_input == 1:
+                y = np.concatenate((y, ['H2S']))
+            print(x)
+            print(y)
+            # Getting dataset
+            xData = updated_df[x].values
+            yData = updated_df[y].values
+
+            # AI Model
+            svr = SVR(kernel='rbf')
+            multiOutputSVR = MultiOutputRegressor(svr)
+            multiOutputSVR = multiOutputSVR.fit(xData, yData)
+
+            global y_pred
+            y_pred = multiOutputSVR.predict(xInput)
+            print(y_pred)
+            controller.show_frame(OutputPage)
 
 class PredictionPage2(tk.Frame):
     def __init__(self, parent, controller):
@@ -268,6 +368,7 @@ class PredictionPage2(tk.Frame):
         doPredictionButton = tk.Button(self, text="DO PREDICTION", fg="#4F3D2F", bg="white", width=13, height=1, bd=0,
                                        command=lambda: controller.show_frame(OutputPage))
         doPredictionButton.grid(column=0, row=0, padx=(350, 80), pady=(520, 20))
+
 
 class HistoryPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -351,4 +452,3 @@ class OutputPage(tk.Frame):
 
 app = tkinterApp()
 app.mainloop()
-
